e5514318f600b7a8b0d0e9058c5dbda744d35855
Razlan, Muhd Afiq Izzudin <muhd.afiq.i.razlan@accenture.com>
2022-12-28 17:18:11 +0800

    Added dependencies for Spring Boot Validation and Sleuth TraceID

--- Orders/src/main/java/com/example/xeTraining/repository/OrderRepository.java
+++ Orders/src/main/java/com/example/xeTraining/repository/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.example.xeTraining.repository;
 
-import com.example.xeTraining.entity.Orders;
+import com.example.xeTraining.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
