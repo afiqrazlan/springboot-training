bbf471c14aa23fe38c0e4d374a30d1be8d690d3e
Razlan, Muhd Afiq Izzudin <muhd.afiq.i.razlan@accenture.com>
2022-12-28 17:18:54 +0800

    Created repository for Order microservice

--- Orders/src/main/java/com/example/xeTraining/repository/OrderRepository.java
+++ Orders/src/main/java/com/example/xeTraining/repository/OrderRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Orders, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long>
+{
 }
